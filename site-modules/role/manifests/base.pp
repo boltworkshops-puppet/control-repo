9710d74db9de7e4190c041000a671d3fc768cee3
Tony Green <tgreen@albatrossflavour.com>
2020-03-07 10:23:21 +1100

    clean up roles and create base role

---
+++ site-modules/role/manifests/base.pp
@@ -0,0 +1,6 @@
+# Class: role::base
+#
+#
+class role::base {
+  include profile::base
+}
---
+++ site-modules/role/manifests/boltworkshop/backend.pp
@@ -0,0 +1,6 @@
+# Class: role::boltworkshop::backend
+#
+#
+class role::boltworkshop::backend {
+  include profile::boltworkshop::users
+}
