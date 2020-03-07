4f657e53b6a2bc6aa5301cc85088629dad47b9e7
Tony Green <tgreen@albatrossflavour.com>
2020-03-07 14:09:06 +1100

    ordering

--- site-modules/profile/manifests/base/repos.pp
+++ site-modules/profile/manifests/base/repos.pp
@@ -2,17 +2,24 @@
 #
 #
 class profile::base::repos {
-  class { 'yum':
-    keep_kernel_devel       => false,
-    clean_old_kernels       => true,
-    manage_os_default_repos => true,
-    notify                  => Exec['yum clean all'],
-  }
+  case $facts['os']['family'] {
+    'Redhat': {
+      Yumrepo <||> -> Package <||>
 
-  # ensure that cache is cleaned out if we update settings
-  exec { 'yum clean all':
-    path        => '/usr/bin/',
-    command     => 'yum clean all',
-    refreshonly => true,
+      class { 'yum':
+        keep_kernel_devel       => false,
+        clean_old_kernels       => true,
+        manage_os_default_repos => true,
+        notify                  => Exec['yum clean all'],
+      }
+
+      # ensure that cache is cleaned out if we update settings
+      exec { 'yum clean all':
+        path        => '/usr/bin/',
+        command     => 'yum clean all',
+        refreshonly => true,
+      }
+    }
+    default: {}
   }
 }
