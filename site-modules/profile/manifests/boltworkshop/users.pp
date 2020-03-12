27f5cddc3960b9dcd99cfa3a09fc70de7620f719
Tony Green <tgreen@albatrossflavour.com>
2020-03-12 21:31:53 +1100

    rand

--- site-modules/profile/manifests/boltworkshop/users.pp
+++ site-modules/profile/manifests/boltworkshop/users.pp
@@ -17,7 +17,7 @@ class profile::boltworkshop::users (
         ensure => present,
       }
 
-      $user_pass = pw_hash(lookup('profile::boltworkshop::users::password',Sensitive,first,undef),6)
+      $user_pass = pw_hash(lookup('profile::boltworkshop::users::password',Sensitive,first,undef),6,fqdn_rand(60))
 
       $users = range('0', $number)
 
